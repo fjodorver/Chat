0225763ba03990321558daebc9682150ed6436b8
Fjodor Versinin <fjodor@vershinin.net>
2017-09-20 11:52:55 +0300

    adopted api for new client

--- src/main/java/net/vershinin/chat/controller/UserController.java
+++ src/main/java/net/vershinin/chat/controller/UserController.java
@@ -2,17 +2,17 @@ package net.vershinin.chat.controller;
 
 import lombok.val;
 import net.vershinin.chat.event.ConnectionEvent;
-import net.vershinin.chat.model.Connection;
-import net.vershinin.chat.model.Connection.Status;
 import net.vershinin.chat.model.User;
 import net.vershinin.chat.service.AuthService;
 import net.vershinin.chat.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.event.EventListener;
 import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.provider.authentication.OAuth2AuthenticationDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -37,37 +37,38 @@ public class UserController {
         this.messagingTemplate = messagingTemplate;
     }
 
-    @GetMapping("/api/v1/users/")
-    public Set<User> getOnlineUsers(){
-        return authService.getOnlineUsers();
+    @GetMapping(value = "/api/v1/me")
+    public User getAuthUser(@AuthenticationPrincipal User user) {
+        return user;
     }
 
     @GetMapping(value = "/signout")
-    public boolean signOut(Authentication authentication){
+    public boolean signOut(Authentication authentication) {
         val details = (OAuth2AuthenticationDetails) authentication.getDetails();
         return authService.logout(details.getTokenValue());
     }
 
     @PostMapping(value = "/signup")
-    public ResponseEntity<User> signUp(@RequestBody User user){
+    public ResponseEntity<User> signUp(@RequestBody User user) {
         userService.save(user);
         return ResponseEntity.ok(user);
     }
 
-    @EventListener
+    @MessageMapping("/getMessages")
     @SendTo("/users/receive")
-    public void onEvent(ConnectionEvent event){
-        val user = userService.findByPrincipal(event.getPrincipal());
-        Connection connection;
-        switch (event.getCommand()){
-            case CONNECT:
-                connection = new Connection(Status.CONNECTED, user);
-                messagingTemplate.convertAndSend("/users/receive", connection);
-                break;
-            case DISCONNECT:
-                connection = new Connection(Status.DISCONNECTED, user);
-                messagingTemplate.convertAndSend("/users/receive", connection);
-                break;
+    public Set<User> getUsers() {
+        return authService.getOnlineUsers();
+    }
+
+    @EventListener()
+    @SendTo("/users/receive")
+    public void onEvent(ConnectionEvent event) {
+        val destination = "/users/receive";
+        val users = authService.getOnlineUsers();
+        switch (event.getCommand()) {
+            case SUBSCRIBE:
+            case UNSUBSCRIBE:
+                messagingTemplate.convertAndSend(destination, users);
         }
     }
 }
